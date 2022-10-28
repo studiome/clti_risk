77f4564d0b0227258a7d2ebae6646ecda05b7646
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-10-28 22:45:40 +0900

    predictor coeffs

--- lib/models/patient_risk.dart
+++ lib/models/patient_risk.dart
@@ -77,3 +77,81 @@ enum GNRIRisk {
   low, // 92<= gnri < 98
   noRisk, // 98 <= gnri
 }
+
+//Covariants Index of 2Yr OS and AFS
+// age less than 65, without CKD, ambulatory(activity),
+// no malignancy are setted to reference.
+enum Covariants {
+  isFemale,
+  age65to74,
+  age75to84,
+  ageOver85,
+  hasCHF,
+  hasCKDG3,
+  hasCKDG4,
+  hasCDKg5,
+  hasCKDg5d,
+  gnriModerate,
+  gnriMajor,
+  activityWheelChair,
+  activityImmobile,
+  pastMalignancy,
+  treatingMalignancy,
+  lesionFP,
+  lesionBelowIP,
+  isUrgent,
+  feverUp,
+  leukocytosis,
+  localInfection,
+}
+
+const OS_H0_Coeff = 0.922;
+
+const OS_Beta_Coeff = [
+  -0.25,
+  0.31,
+  0.76,
+  1.04,
+  0.50,
+  0.27,
+  0.61,
+  0.76,
+  1.01,
+  0.14,
+  0.52,
+  0.28,
+  0.77,
+  0.20,
+  0.56,
+  -0.07,
+  0.16,
+  0.0,
+  0.0,
+  0.0,
+  0.0,
+];
+const AFS_H0_Coeff = 0.876;
+
+const AFS_Beta_Coeff = [
+  -0.21,
+  0.19,
+  0.42,
+  0.62,
+  0.10,
+  0.16,
+  0.36,
+  0.73,
+  0.81,
+  0.09,
+  0.45,
+  0.37,
+  0.78,
+  0.15,
+  0.39,
+  0.34,
+  0.36,
+  0.19,
+  0.15,
+  -0.07,
+  0.15,
+];
