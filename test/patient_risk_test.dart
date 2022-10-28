4c1332b262655c0420bae1f3c8a533f69b5c39f9
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-10-28 14:35:03 +0900

    test init

---
+++ test/patient_risk_test.dart
@@ -0,0 +1,18 @@
+import 'package:clti_risk/models/patient_data.dart';
+import 'package:clti_risk/models/patient_risk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('GNRI major', () {
+    final pd = PatientData(
+        sex: Sex.male,
+        age: 70,
+        height: 1.75,
+        weight: 50.0,
+        alb: 3.0,
+        activity: Activity.wheelchair);
+    final pr = PatientRisk(patientData: pd);
+    expect(pr.gnri.toStringAsFixed(1), '75.6');
+    expect(pr.gnriRisk, GNRIRisk.major);
+  });
+}
