5d69fcfb0eba8ad074e775da609cd29770bff174
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-10-29 23:22:44 +0900

    common app scaffold

---
+++ lib/widgets/app_scaffold.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final String title;
+  final Widget child;
+  const AppScaffold({super.key, required this.title, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: child,
+    );
+  }
+}
