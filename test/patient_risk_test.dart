a2f0645eb390f06493c505ca70ee321c54f1faee
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-10-29 12:54:34 +0900

    low risk case passed

--- test/patient_risk_test.dart
+++ test/patient_risk_test.dart
@@ -21,55 +21,8 @@ void main() {
 
     _testCase(pd, want);
   });
-  test('GNRI major', () {
-    final pd = PatientData(
-        sex: Sex.male,
-        age: 70,
-        height: 1.75,
-        weight: 50.0,
-        alb: 3.0,
-        activity: Activity.wheelchair);
-    final pr = PatientRisk(patientData: pd);
-    expect(pr.gnri.toStringAsFixed(1), '75.6');
-    expect(pr.gnriRisk, GNRIRisk.major);
-  });
-  test('GNRI moderate', () {
-    final pd = PatientData(
-        sex: Sex.female,
-        age: 65,
-        height: 1.55,
-        weight: 40.0,
-        alb: 3.8,
-        activity: Activity.ambulatory);
-    final pr = PatientRisk(patientData: pd);
-    expect(pr.gnri.toStringAsFixed(1), '88.1');
-    expect(pr.gnriRisk, GNRIRisk.moderate);
-  });
-  test('GNRI low', () {
-    final pd = PatientData(
-        sex: Sex.female,
-        age: 60,
-        height: 1.45,
-        weight: 30.0,
-        alb: 4.5,
-        activity: Activity.ambulatory);
-    final pr = PatientRisk(patientData: pd);
-    expect(pr.gnri.toStringAsFixed(1), '94.1');
-    expect(pr.gnriRisk, GNRIRisk.low);
-  });
-  test('GNRI no risk', () {
-    final pd = PatientData(
-        sex: Sex.female,
-        age: 60,
-        height: 1.65,
-        weight: 60.0,
-        alb: 4.0,
-        activity: Activity.ambulatory);
-    final pr = PatientRisk(patientData: pd);
-    expect(pr.gnri.toStringAsFixed(1), '101.3');
-    expect(pr.gnriRisk, GNRIRisk.noRisk);
-  });
-  test("OS case1", () {
+
+  test('Low Risk Case', () {
     final pd = PatientData(
         sex: Sex.male,
         age: 50,
@@ -86,10 +39,15 @@ void main() {
       ..hasFeverUp = true
       ..hasLeukocytosis = true
       ..hasLocalInfection = true;
-    final pr = PatientRisk(patientData: pd);
-    expect((pr.predictedOS * 100.0).round(), 91);
-    expect((pr.predictedAFS * 100).round(), 64);
-    expect(pr.osRisk, OSRisk.low);
+
+    const want = _WantData(
+        gnri: '101.3',
+        gnriRisk: GNRIRisk.noRisk,
+        predictedOS: '0.91',
+        predictedAFS: '0.64',
+        osRisk: OSRisk.low);
+
+    _testCase(pd, want);
   });
 }
 
