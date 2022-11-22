16573fa5b5eb7ccf2cb72493e768e4d6691fc698
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-22 15:04:20 +0900

    Question Binder needs .builder constructor

--- lib/widgets/question_binder.dart
+++ lib/widgets/question_binder.dart
@@ -1,7 +1,12 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+import 'question_page.dart';
 
 class QuestionBinder extends StatelessWidget {
-  const QuestionBinder({super.key});
+  final List<Tab> tabBars;
+  final List<QuestionPage> questionPages;
+  const QuestionBinder(
+      {super.key, required this.tabBars, required this.questionPages});
 
   @override
   Widget build(BuildContext context) {
