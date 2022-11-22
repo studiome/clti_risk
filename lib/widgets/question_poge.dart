ed46d6b0ec17037f36bbc8e4a679f43fd3357aea
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-22 14:42:09 +0900

    tab page skelton

---
+++ lib/widgets/question_poge.dart
@@ -0,0 +1,33 @@
+import 'package:clti_risk/widgets/tab_transition_navigator.dart';
+import 'package:flutter/material.dart';
+
+class QuestionPage extends StatelessWidget {
+  final int tabIndex;
+  final int tabCount;
+  final Widget content;
+  final String title;
+  final String subtitle;
+  const QuestionPage(
+      {super.key,
+      required this.title,
+      required this.content,
+      required this.subtitle,
+      required this.tabIndex,
+      required this.tabCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(subtitle),
+          content,
+          TabTransitionNavigator(
+            tabIndex: tabIndex,
+            tabCount: tabCount,
+          ),
+        ],
+      ),
+    );
+  }
+}
