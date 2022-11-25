836036930761d7719e82871e66bb0528c800eaf0
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-25 11:17:32 +0900

    multiple choice question, test passed

--- test/widgets/question_content_test.dart
+++ test/widgets/question_content_test.dart
@@ -1,3 +1,4 @@
+import 'package:clti_risk/models/clinical_data_controller.dart';
 import 'package:clti_risk/models/patient_data.dart';
 import 'package:clti_risk/models/questions.dart';
 import 'package:clti_risk/widgets/question_content.dart';
@@ -6,20 +7,16 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('MultipleChoice', (tester) async {
-    var pd = PatientData();
+    final pd = PatientData();
     await tester.pumpWidget(MaterialApp(
-        home: Scaffold(
-      body: MultipleQuestionContent(
-        question: Questions.sex,
-        dataItem: pd.sex,
-        values: Sex.values,
-        itemHeight: 60.0,
-        itemWidth: 160.0,
-        tabIndex: Questions.sex.index,
-        tabCount: Questions.values.length,
-        onChanged: null,
+      home: Scaffold(
+        body: ClinicalDataController(
+            patientData: pd,
+            child: ChoiceTestWidget(
+              patientData: pd,
+            )),
       ),
-    )));
+    ));
 
     expect(pd.sex, Sex.female);
     final choice0 = find.text(Sex.values[0].toString()); // Male
@@ -34,4 +31,37 @@ void main() {
     await tester.pumpAndSettle();
     expect(pd.sex, Sex.male);
   });
+}
+
+class ChoiceTestWidget extends StatelessWidget {
+  final PatientData patientData;
+  const ChoiceTestWidget({super.key, required this.patientData});
+
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    final d = c.data;
+    return Scaffold(
+        body: ClinicalDataController(
+            patientData: patientData,
+            child: ValueListenableBuilder(
+              valueListenable: d,
+              builder: (context, value, _) {
+                return MultipleQuestionContent(
+                  question: Questions.sex,
+                  values: Sex.values,
+                  dataItem: value.sex,
+                  itemHeight: 60.0,
+                  itemWidth: 160.0,
+                  tabIndex: Questions.sex.index,
+                  tabCount: Questions.values.length,
+                  onChanged: (v) {
+                    if (v == null) return;
+                    value.sex = v;
+                  },
+                );
+              },
+            )));
+  }
 }
