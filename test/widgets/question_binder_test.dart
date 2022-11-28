ae0de12dd8d77cb2bec3a52df8557917754d860d
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-28 13:18:06 +0900

    refactor test

--- test/widgets/question_binder_test.dart
+++ test/widgets/question_binder_test.dart
@@ -6,73 +6,85 @@ import 'package:flutter_test/flutter_test.dart';
 const int tabCount = 3;
 
 void main() {
-  testWidgets('Build check', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: QuestionPagesTestWidget()));
-    final title = find.text('Tab0');
-    final subtitle = find.text('TabTest0');
-    final content = find.text('Q-0');
-    final nextButton = find.text('Next');
-    final backButton = find.text('Back');
-    expect(title, findsOneWidget);
-    expect(subtitle, findsOneWidget);
-    expect(content, findsOneWidget);
-    expect(nextButton, findsOneWidget);
-    expect(backButton, findsOneWidget);
-  });
-  testWidgets('Back is disabled at first tab', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: QuestionPagesTestWidget()));
-    final title = find.text('Tab0');
-    final subtitle = find.text('TabTest0');
-    final content = find.text('Q-0');
-    final backButton = find.text('Back');
-    await tester.tap(backButton);
-    await tester.pumpAndSettle();
-    expect(title, findsOneWidget);
-    expect(subtitle, findsOneWidget);
-    expect(content, findsOneWidget);
-  });
-  testWidgets('Next', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: QuestionPagesTestWidget()));
-    final nextButton = find.text('Next');
-    await tester.tap(nextButton);
-    await tester.pumpAndSettle();
-    final title = find.text('Tab1');
-    final subtitle = find.text('TabTest1');
-    final content = find.text('Q-1');
-    expect(title, findsOneWidget);
-    expect(subtitle, findsOneWidget);
-    expect(content, findsOneWidget);
-  });
-  testWidgets('Next is disabled at last tab', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: QuestionPagesTestWidget()));
-    final nextButton = find.text('Next');
-    await tester.tap(nextButton);
-    await tester.pumpAndSettle(); // Tab1
-    await tester.tap(nextButton);
-    await tester.pumpAndSettle(); // Tab 2
-    await tester.tap(nextButton);
-    await tester.pumpAndSettle();
-    final title = find.text('Tab2');
-    final subtitle = find.text('TabTest2');
-    final content = find.text('Q-2');
-    expect(title, findsOneWidget);
-    expect(subtitle, findsOneWidget);
-    expect(content, findsOneWidget);
-  });
-  testWidgets('Go and Back', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: QuestionPagesTestWidget()));
-    final nextButton = find.text('Next');
-    final backButton = find.text('Back');
-    await tester.tap(nextButton);
-    await tester.pumpAndSettle();
-    await tester.tap(backButton);
-    await tester.pumpAndSettle();
-    final title = find.text('Tab0');
-    final subtitle = find.text('TabTest0');
-    final content = find.text('Q-0');
-    expect(title, findsOneWidget);
-    expect(subtitle, findsOneWidget);
-    expect(content, findsOneWidget);
+  group('Question Binder Test', () {
+    late MaterialApp testApp;
+    late Finder titleAt0;
+    late Finder subtitleAt0;
+    late Finder contentAt0;
+    late Finder titleAt1;
+    late Finder subtitleAt1;
+    late Finder contentAt1;
+    late Finder titleAt2;
+    late Finder subtitleAt2;
+    late Finder contentAt2;
+    late Finder nextButton;
+    late Finder backButton;
+    setUp(() {
+      testApp = MaterialApp(home: QuestionPagesTestWidget());
+      titleAt0 = find.text('Tab0');
+      subtitleAt0 = find.text('TabTest0');
+      contentAt0 = find.text('Q-0');
+      titleAt1 = find.text('Tab1');
+      subtitleAt1 = find.text('TabTest1');
+      contentAt1 = find.text('Q-1');
+      titleAt2 = find.text('Tab2');
+      subtitleAt2 = find.text('TabTest2');
+      contentAt2 = find.text('Q-2');
+
+      nextButton = find.text('Next');
+      backButton = find.text('Back');
+    });
+
+    testWidgets('build check', (tester) async {
+      await tester.pumpWidget(testApp);
+      expect(titleAt0, findsOneWidget);
+      expect(subtitleAt0, findsOneWidget);
+      expect(contentAt0, findsOneWidget);
+      expect(nextButton, findsOneWidget);
+      expect(backButton, findsOneWidget);
+    });
+
+    testWidgets('back button is disabled at first tab', (tester) async {
+      await tester.pumpWidget(testApp);
+      await tester.tap(backButton);
+      await tester.pumpAndSettle();
+      expect(titleAt0, findsOneWidget);
+      expect(subtitleAt0, findsOneWidget);
+      expect(contentAt0, findsOneWidget);
+    });
+
+    testWidgets('go to next', (tester) async {
+      await tester.pumpWidget(testApp);
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+      expect(titleAt1, findsOneWidget);
+      expect(subtitleAt1, findsOneWidget);
+      expect(contentAt1, findsOneWidget);
+    });
+
+    testWidgets('next button is disabled at last tab', (tester) async {
+      await tester.pumpWidget(testApp);
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle(); // Tab1
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle(); // Tab 2
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+      expect(titleAt2, findsOneWidget);
+      expect(subtitleAt2, findsOneWidget);
+      expect(contentAt2, findsOneWidget);
+    });
+
+    testWidgets('go back', (tester) async {
+      await tester.pumpWidget(testApp);
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+      await tester.tap(backButton);
+      await tester.pumpAndSettle();
+      expect(titleAt0, findsOneWidget);
+      expect(subtitleAt0, findsOneWidget);
+      expect(contentAt0, findsOneWidget);
+    });
   });
 }
 
