a1c7ceb40a3357b7f850b431338306082c19f2c0
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:30:51 +0900

    contra

---
+++ lib/widgets/contralateral_question_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class ContraLateralQuestionPage extends StatefulWidget {
+  const ContraLateralQuestionPage({super.key});
+
+  @override
+  State<ContraLateralQuestionPage> createState() =>
+      _ContraLateralQuestionPageState();
+}
+
+class _ContraLateralQuestionPageState extends State<ContraLateralQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.contralateral,
+      values: YesNo.values,
+      dataItem: c.patientData.hasContraLateralLesion.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.contralateral.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasContraLateralLesion = v.toBool();
+        });
+      },
+    );
+  }
+}
