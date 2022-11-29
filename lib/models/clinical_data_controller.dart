40ea5d99b81eba45524e9f76067ad1ebdf24d0e4
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-29 15:28:29 +0900

    omit value notifier beacause of using instance value

--- lib/models/clinical_data_controller.dart
+++ lib/models/clinical_data_controller.dart
@@ -1,18 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'patient_data.dart';
 
 class ClinicalDataController extends InheritedWidget {
-  final ValueNotifier<PatientData> data;
   final PatientData patientData;
-  final PatientDataNotifier notifier;
 
-  ClinicalDataController(
+  const ClinicalDataController(
       {super.key, required this.patientData, required Widget child})
-      : data = ValueNotifier(patientData),
-        notifier = PatientDataNotifier(patientData),
-        super(child: child);
+      : super(child: child);
 
   static ClinicalDataController? of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<ClinicalDataController>();
@@ -22,17 +17,4 @@ class ClinicalDataController extends InheritedWidget {
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     return true;
   }
-}
-
-class PatientDataNotifier extends ChangeNotifier
-    implements ValueListenable<PatientData> {
-  final PatientData _value;
-  PatientDataNotifier(this._value);
-
-  void update() {
-    notifyListeners();
-  }
-
-  @override
-  PatientData get value => _value;
 }
