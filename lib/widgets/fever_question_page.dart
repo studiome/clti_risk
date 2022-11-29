2916376f01d5be147ff597cbd961439b793678bb
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:36:47 +0900

    Fever

---
+++ lib/widgets/fever_question_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class FeverQuestionPage extends StatefulWidget {
+  const FeverQuestionPage({super.key});
+
+  @override
+  State<FeverQuestionPage> createState() => _FeverQuestionPageState();
+}
+
+class _FeverQuestionPageState extends State<FeverQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.fever,
+      values: YesNo.values,
+      dataItem: c.patientData.hasFever.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.fever.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasFever = v.toBool();
+        });
+      },
+    );
+  }
+}
