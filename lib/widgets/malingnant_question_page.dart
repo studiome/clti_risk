703c5cce2bb9a15e92794de68624f5dd0003e9ad
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:42:29 +0900

    malignancy

---
+++ lib/widgets/malingnant_question_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/patient_data.dart';
+import '../models/questions.dart';
+import 'question_page.dart';
+
+class MalignantQuestionPage extends StatefulWidget {
+  const MalignantQuestionPage({super.key});
+
+  @override
+  State<MalignantQuestionPage> createState() => _MalignantQuestionPageState();
+}
+
+class _MalignantQuestionPageState extends State<MalignantQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<MalignantNeoplasm>(
+      question: Questions.malignantNeoplasm,
+      values: MalignantNeoplasm.values,
+      dataItem: c.patientData.malignant,
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.malignantNeoplasm.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.malignant = v;
+        });
+      },
+    );
+  }
+}
