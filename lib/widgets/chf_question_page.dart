0647f05f7fe9987d020d82b9d8035e019415248e
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:29:37 +0900

    CHF

---
+++ lib/widgets/chf_question_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class CHFQuestionPage extends StatefulWidget {
+  const CHFQuestionPage({super.key});
+
+  @override
+  State<CHFQuestionPage> createState() => _CHFQuestionPageState();
+}
+
+class _CHFQuestionPageState extends State<CHFQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.chf,
+      values: YesNo.values,
+      dataItem: c.patientData.hasCHF.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.chf.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasCHF = v.toBool();
+        });
+      },
+    );
+  }
+}
