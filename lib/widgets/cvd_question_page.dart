5920027989b53eaf7d434f79ed06033a17b2f685
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:31:56 +0900

    CVD

---
+++ lib/widgets/cvd_question_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class CVDQuestionPage extends StatefulWidget {
+  const CVDQuestionPage({super.key});
+
+  @override
+  State<CVDQuestionPage> createState() => _CVDQuestionPageState();
+}
+
+class _CVDQuestionPageState extends State<CVDQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.cvd,
+      values: YesNo.values,
+      dataItem: c.patientData.hasCVD.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.cvd.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasCVD = v.toBool();
+        });
+      },
+    );
+  }
+}
