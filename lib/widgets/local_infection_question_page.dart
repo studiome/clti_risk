603b3719e62766b48a79dbc469d73470d8f94724
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 08:38:52 +0900

    local infection

---
+++ lib/widgets/local_infection_question_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class LocalInfectionQuestionPage extends StatefulWidget {
+  const LocalInfectionQuestionPage({super.key});
+
+  @override
+  State<LocalInfectionQuestionPage> createState() =>
+      _LocalInfectionQuestionPageState();
+}
+
+class _LocalInfectionQuestionPageState
+    extends State<LocalInfectionQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.localInfection,
+      values: YesNo.values,
+      dataItem: c.patientData.hasLocalInfection.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.localInfection.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasLocalInfection = v.toBool();
+        });
+      },
+    );
+  }
+}
