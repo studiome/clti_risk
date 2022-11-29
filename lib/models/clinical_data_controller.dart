d97125a69011a08f0551f3a2028723f51cd11a2b
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-29 15:29:19 +0900

    Revert "omit value notifier beacause of using instance value"
    This reverts commit 40ea5d99b81eba45524e9f76067ad1ebdf24d0e4.

--- lib/models/clinical_data_controller.dart
+++ lib/models/clinical_data_controller.dart
@@ -1,13 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'patient_data.dart';
 
 class ClinicalDataController extends InheritedWidget {
+  final ValueNotifier<PatientData> data;
   final PatientData patientData;
+  final PatientDataNotifier notifier;
 
-  const ClinicalDataController(
+  ClinicalDataController(
       {super.key, required this.patientData, required Widget child})
-      : super(child: child);
+      : data = ValueNotifier(patientData),
+        notifier = PatientDataNotifier(patientData),
+        super(child: child);
 
   static ClinicalDataController? of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<ClinicalDataController>();
@@ -17,4 +22,17 @@ class ClinicalDataController extends InheritedWidget {
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     return true;
   }
+}
+
+class PatientDataNotifier extends ChangeNotifier
+    implements ValueListenable<PatientData> {
+  final PatientData _value;
+  PatientDataNotifier(this._value);
+
+  void update() {
+    notifyListeners();
+  }
+
+  @override
+  PatientData get value => _value;
 }
