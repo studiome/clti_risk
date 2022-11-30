d24aadf7a27a3cc9987e5c094efd353dd36e2f2c
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 10:02:42 +0900

    abnormal WBC

---
+++ lib/widgets/wbc_question_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class AbnormalWBCQuestionPage extends StatefulWidget {
+  const AbnormalWBCQuestionPage({super.key});
+
+  @override
+  State<AbnormalWBCQuestionPage> createState() =>
+      _AbnormalWBCQuestionPageState();
+}
+
+class _AbnormalWBCQuestionPageState extends State<AbnormalWBCQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.abnormalWBC,
+      values: YesNo.values,
+      dataItem: c.patientData.hasAbnormalWBC.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.abnormalWBC.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.hasAbnormalWBC = v.toBool();
+        });
+      },
+    );
+  }
+}
