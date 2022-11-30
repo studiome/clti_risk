a0cbda17a66af24b951a37f7841420035465d12c
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 13:36:41 +0900

    weight

---
+++ lib/widgets/weight_question_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import 'question_page.dart';
+
+class WeightQuestionPage extends StatelessWidget {
+  final TextEditingController controller;
+  const WeightQuestionPage({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return NumberFormQuestionContent(
+        question: Questions.weight,
+        formController: controller,
+        isDecimal: true,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.allow(RegExp(r'^\d{1,3}\.?\d{0,1}')),
+        ],
+        onSubmitted: (v) {
+          try {
+            c.patientData.weight = double.parse(v);
+          } catch (e) {
+            c.patientData.weight = null;
+          }
+        },
+        itemWidth: 240,
+        itemHeight: 60,
+        tabIndex: Questions.weight.index,
+        tabCount: Questions.values.length);
+  }
+}
