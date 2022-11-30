eee77b213f6edaf653c812507ee0a1d5a836aff5
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-11-30 10:00:51 +0900

    urgent

---
+++ lib/widgets/urgent_question_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../models/clinical_data_controller.dart';
+import '../models/questions.dart';
+import '../models/yes_no.dart';
+import 'question_page.dart';
+
+class UrgentQuestionPage extends StatefulWidget {
+  const UrgentQuestionPage({super.key});
+
+  @override
+  State<UrgentQuestionPage> createState() => _UrgentQuestionPageState();
+}
+
+class _UrgentQuestionPageState extends State<UrgentQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    final c = ClinicalDataController.of(context);
+    if (c == null) throw NullThrownError();
+    return MultipleQuestionPage<YesNo>(
+      question: Questions.urgentProcedure,
+      values: YesNo.values,
+      dataItem: c.patientData.isUrgent.toYesNo(),
+      itemWidth: 180.0,
+      itemHeight: 60.0,
+      tabIndex: Questions.urgentProcedure.index,
+      tabCount: Questions.values.length,
+      onChanged: (v) {
+        if (v == null) return;
+        setState(() {
+          c.patientData.isUrgent = v.toBool();
+        });
+      },
+    );
+  }
+}
