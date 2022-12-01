8d8ddd92e11a667ebe0b7d0b3f2248b2346f3b6e
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-01 15:04:48 +0900

    inherited widgets include risk object, could change this design

--- lib/models/clinical_data_controller.dart
+++ lib/models/clinical_data_controller.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'patient_data.dart';
+import 'patient_risk.dart';
 
 class ClinicalDataController extends InheritedWidget {
   final PatientData patientData;
+  final PatientRisk? risk;
 
   const ClinicalDataController(
-      {super.key, required this.patientData, required Widget child})
+      {super.key,
+      required this.patientData,
+      required this.risk,
+      required Widget child})
       : super(child: child);
 
   static ClinicalDataController? of(BuildContext context) {
