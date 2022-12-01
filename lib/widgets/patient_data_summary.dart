5d48efdd87b6001eb5551e125248fd5b61f59b80
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-01 15:00:31 +0900

    data summary, calculation widget

---
+++ lib/widgets/patient_data_summary.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PatientDataSummary extends StatelessWidget {
+  const PatientDataSummary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
