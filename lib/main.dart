801f16ffcead4df6d388e21c0fc3ff3b0d47c57a
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-10 22:12:55 +0900

    init i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'models/clinical_data_controller.dart';
 import 'models/patient_data.dart';
@@ -78,6 +79,14 @@ class _AppRootState extends State<AppRoot> {
             //fontFamily: 'Noto Sans JP',
           ),
           themeMode: ThemeMode.system,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'),
+            Locale('ja'),
+          ],
           home: ClinicalDataController(
               patientData: pd,
               onRiskCalculated: onRiskCalculated,
