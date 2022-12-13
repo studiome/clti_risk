f9dc2af0b1ce840211fb5699b276b45b2abfc6b6
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-13 15:53:25 +0900

    instruction content, en, jp only

--- lib/widgets/instruction_page.dart
+++ lib/widgets/instruction_page.dart
@@ -16,18 +16,38 @@ class InstructionPage extends StatelessWidget {
           child: RichText(
               text: TextSpan(
             style: Theme.of(context).textTheme.bodyText1,
-            children: const [
-              TextSpan(
-                  text:
-                      'i. Check the choices or fill in the blanks, then press "Next".\nii. Analysis button will show you patient risks.\niii. If you would like to see summary, press '),
-              WidgetSpan(child: Icon(Icons.summarize_outlined)),
-              TextSpan(text: ' above.\niv. You can refresh data to press '),
-              WidgetSpan(child: Icon(Icons.refresh_outlined)),
-              TextSpan(text: ' next to summary button.\n'),
-            ],
+            children: _getInstructionContent(context),
           ))),
       tabIndex: Questions.instruction.index,
       tabCount: Questions.values.length,
     );
+  }
+
+  List<InlineSpan>? _getInstructionContent(BuildContext context) {
+    final locale = Localizations.localeOf(context);
+    switch (locale.languageCode) {
+      case 'ja':
+        return <InlineSpan>[
+          const TextSpan(
+              text:
+                  'i. 選択肢を選ぶか値を入力して, 次へを押してください。\nii. 全て入力し、リスク解析ボタンを押すと結果が得られます。\niii. データ一覧が見たい場合は、上の'),
+          const WidgetSpan(child: Icon(Icons.summarize_outlined)),
+          const TextSpan(text: ' を押してください。\niv. 初期化したい場合は、上の'),
+          const WidgetSpan(child: Icon(Icons.refresh_outlined)),
+          const TextSpan(text: 'を押してください。\n'),
+        ];
+        break;
+      case 'en':
+        return <InlineSpan>[
+          const TextSpan(
+              text:
+                  'i. Check the choices or fill in the blanks, then press "Next".\nii. Analysis button will show you patient risks.\niii. If you would like to see summary, press '),
+          const WidgetSpan(child: Icon(Icons.summarize_outlined)),
+          const TextSpan(text: ' above.\niv. You can refresh data to press '),
+          const WidgetSpan(child: Icon(Icons.refresh_outlined)),
+          const TextSpan(text: ' next to summary button.\n'),
+        ];
+    }
+    return null;
   }
 }
