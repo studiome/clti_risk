0270c8397fc6abdaa2b1fb8696f2cf53c3e65d68
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2022-12-16 23:31:27 +0900

    risk view ja/en test

--- test/widgets/risk_view_test.dart
+++ test/widgets/risk_view_test.dart
@@ -1,3 +1,4 @@
+import 'package:clti_risk/models/locale_controller.dart';
 import 'package:clti_risk/models/patient_data.dart';
 import 'package:clti_risk/models/patient_risk.dart';
 import 'package:clti_risk/widgets/risk_view.dart';
@@ -7,9 +8,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   late PatientData pd;
-  group('Risk Viewer Test, [locale:en] ', () {
+  late LocaleController localeController;
+  group('Risk Viewer Test ', () {
     setUp(() {
       pd = PatientData();
+      localeController = LocaleController(const Locale('en'));
     });
 
     testWidgets('normal case,', (tester) async {
@@ -20,7 +23,7 @@ void main() {
         ..alb = 4.0;
       await tester.pumpWidget(TestApp(
         risk: PatientRisk(patientData: pd),
-        locale: const Locale('en'),
+        localeController: localeController,
       ));
       await tester.pumpAndSettle();
       // GNRI
@@ -30,9 +33,9 @@ void main() {
       // 30D Amputation/Death
       expect(find.text('1.3%'), findsOneWidget);
       // 30D MALE
+      expect(find.text('3.2%'), findsOneWidget);
 
       //drag
-      expect(find.text('3.2%'), findsOneWidget);
       await tester.dragUntilVisible(find.text('Predicted 2-year OS'),
           find.byType(ListView), const Offset(0.0, -100.0));
       await tester.pumpAndSettle();
@@ -43,6 +46,15 @@ void main() {
       expect(find.text('Low Risk'), findsOneWidget);
       // 2y AFS
       expect(find.text('88%'), findsOneWidget);
+
+      //ja
+      localeController.value = const Locale('ja');
+      await tester.pumpAndSettle();
+      expect(find.textContaining('低リスク'), findsOneWidget);
+      await tester.dragUntilVisible(find.textContaining('GNRI'),
+          find.byType(ListView), const Offset(0.0, 100.0));
+      await tester.pumpAndSettle();
+      expect(find.textContaining('リスクなし'), findsOneWidget);
     });
 
     testWidgets('low risk case,', (tester) async {
@@ -73,7 +85,7 @@ void main() {
 
       await tester.pumpWidget(TestApp(
         risk: PatientRisk(patientData: pd),
-        locale: const Locale('en'),
+        localeController: localeController,
       ));
       await tester.pumpAndSettle();
       // GNRI
@@ -82,9 +94,10 @@ void main() {
       expect(find.text('No Risk'), findsOneWidget);
       // 30D Amputation/Death
       expect(find.text('8.8%'), findsOneWidget);
-      //drag
-      expect(find.text('15.2%'), findsOneWidget);
       // 30D MALE
+      expect(find.text('15.2%'), findsOneWidget);
+
+      //drag
       await tester.dragUntilVisible(find.text('Predicted 2-year OS'),
           find.byType(ListView), const Offset(0.0, -100.0));
       await tester.pumpAndSettle();
@@ -95,6 +108,15 @@ void main() {
       expect(find.text('Low Risk'), findsOneWidget);
       // 2y AFS
       expect(find.text('64%'), findsOneWidget);
+
+      //ja
+      localeController.value = const Locale('ja');
+      await tester.pumpAndSettle();
+      expect(find.textContaining('低リスク'), findsOneWidget);
+      await tester.dragUntilVisible(find.textContaining('GNRI'),
+          find.byType(ListView), const Offset(0.0, 100.0));
+      await tester.pumpAndSettle();
+      expect(find.textContaining('リスクなし'), findsOneWidget);
     });
 
     testWidgets('medium risk case,', (tester) async {
@@ -125,7 +147,128 @@ void main() {
 
       await tester.pumpWidget(TestApp(
         risk: PatientRisk(patientData: pd),
-        locale: const Locale('en'),
+        localeController: localeController,
+      ));
+      await tester.pumpAndSettle();
+      // GNRI
+      expect(find.text('93.8'), findsOneWidget);
+      // GNRI risk
+      expect(find.text('Low Risk'), findsOneWidget);
+      // 30D Amputation/Death
+      expect(find.text('17.0%'), findsOneWidget);
+      //drag
+      expect(find.text('17.5%'), findsOneWidget);
+      // 30D MALE
+      await tester.dragUntilVisible(find.text('Predicted 2-year OS'),
+          find.byType(ListView), const Offset(0.0, -100.0));
+      await tester.pumpAndSettle();
+
+      // 2y OS
+      expect(find.text('67%'), findsOneWidget);
+      // 2y OS risk
+      expect(find.text('Medium Risk'), findsOneWidget);
+      // 2y AFS
+      expect(find.text('25%'), findsOneWidget);
+
+      //ja
+      localeController.value = const Locale('ja');
+      await tester.pumpAndSettle();
+      expect(find.textContaining('中等度リスク'), findsOneWidget);
+      await tester.dragUntilVisible(find.textContaining('GNRI'),
+          find.byType(ListView), const Offset(0.0, 100.0));
+      await tester.pumpAndSettle();
+      expect(find.textContaining('軽度栄養リスク'), findsOneWidget);
+    });
+
+    testWidgets('high risk case,', (tester) async {
+      pd
+        ..sex = Sex.male
+        ..age = 85
+        ..height = 1.75
+        ..weight = 55.1
+        ..alb = 3.5
+        ..activity = Activity.immobile
+        ..hasCHF = false
+        ..hasCVD = false
+        ..ckd = CKD.g5
+        ..malignant = MalignantNeoplasm.underTreatment
+        ..hasAILesion = false
+        ..hasFPLesion = false
+        ..hasBKLesion = true
+        ..isUrgent = true
+        ..hasFever = false
+        ..hasAbnormalWBC = true
+        ..hasLocalInfection = false
+        ..hasCAD = true
+        ..hasDyslipidemia = true
+        ..isSmoking = true
+        ..hasContraLateralLesion = true
+        ..hasOtherVD = false
+        ..rutherford = RutherfordClassification.class5;
+
+      await tester.pumpWidget(TestApp(
+        risk: PatientRisk(patientData: pd),
+        localeController: localeController,
+      ));
+      await tester.pumpAndSettle();
+      // GNRI
+      expect(find.text('86.2'), findsOneWidget);
+      // GNRI risk
+      expect(find.text('Moderate Risk'), findsOneWidget);
+      // 30D Amputation/Death
+      expect(find.text('10.0%'), findsOneWidget);
+      //drag
+      expect(find.text('4.3%'), findsOneWidget);
+      // 30D MALE
+      await tester.dragUntilVisible(find.text('Predicted 2-year OS'),
+          find.byType(ListView), const Offset(0.0, -100.0));
+      await tester.pumpAndSettle();
+
+      // 2y OS
+      expect(find.text('8%'), findsOneWidget);
+      // 2y OS risk
+      expect(find.text('High Risk'), findsOneWidget);
+      // 2y AFS
+      expect(find.text('3%'), findsOneWidget);
+      //ja
+      localeController.value = const Locale('ja');
+      await tester.pumpAndSettle();
+      expect(find.textContaining('高リスク'), findsOneWidget);
+      await tester.dragUntilVisible(find.textContaining('GNRI'),
+          find.byType(ListView), const Offset(0.0, 100.0));
+      await tester.pumpAndSettle();
+      expect(find.textContaining('中等度栄養リスク'), findsOneWidget);
+    });
+
+    testWidgets('medium risk case,', (tester) async {
+      pd
+        ..sex = Sex.female
+        ..age = 70
+        ..height = 1.53
+        ..weight = 55.0
+        ..alb = 3.5
+        ..activity = Activity.wheelchair
+        ..hasCHF = true
+        ..hasCVD = true
+        ..ckd = CKD.g4
+        ..malignant = MalignantNeoplasm.pastHistory
+        ..hasAILesion = false
+        ..hasFPLesion = true
+        ..hasBKLesion = true
+        ..isUrgent = true
+        ..hasFever = true
+        ..hasAbnormalWBC = true
+        ..hasLocalInfection = true
+        ..hasCAD = false
+        ..hasDyslipidemia = true
+        ..isSmoking = false
+        ..hasContraLateralLesion = true
+        ..hasOtherVD = false
+        ..rutherford = RutherfordClassification.class5;
+
+      await tester.pumpWidget(TestApp(
+        risk: PatientRisk(patientData: pd),
+        localeController: localeController,
       ));
       await tester.pumpAndSettle();
       // GNRI
@@ -177,7 +320,7 @@ void main() {
 
       await tester.pumpWidget(TestApp(
         risk: PatientRisk(patientData: pd),
-        locale: const Locale('en'),
+        localeController: localeController,
       ));
       await tester.pumpAndSettle();
       // GNRI
@@ -195,10 +338,6 @@ void main() {
 
       // 2y OS
       expect(find.text('8%'), findsOneWidget);
-      // 2y OS risk
-      expect(find.text('High Risk'), findsOneWidget);
-      // 2y AFS
-      expect(find.text('3%'), findsOneWidget);
     });
 
     testWidgets('high risk case 2,', (tester) async {
@@ -229,7 +368,7 @@ void main() {
 
       await tester.pumpWidget(TestApp(
         risk: PatientRisk(patientData: pd),
-        locale: const Locale('en'),
+        localeController: localeController,
       ));
       await tester.pumpAndSettle();
       // GNRI
@@ -249,23 +388,38 @@ void main() {
       expect(find.text('0%'), findsNWidgets(2));
       // 2y OS risk
       expect(find.text('High Risk'), findsOneWidget);
+
+      //ja
+      localeController.value = const Locale('ja');
+      await tester.pumpAndSettle();
+      expect(find.textContaining('高リスク'), findsOneWidget);
+      await tester.dragUntilVisible(find.textContaining('GNRI'),
+          find.byType(ListView), const Offset(0.0, 100.0));
+      await tester.pumpAndSettle();
+      expect(find.textContaining('高度栄養リスク'), findsOneWidget);
     });
   });
 }
 
 class TestApp extends StatelessWidget {
   final PatientRisk risk;
-  final Locale locale;
-  const TestApp({super.key, required this.risk, required this.locale});
+  final LocaleController localeController;
+  const TestApp(
+      {super.key, required this.risk, required this.localeController});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        locale: locale,
-        home: RiskView(
-          risk: risk,
-        ));
+    return ValueListenableBuilder(
+        valueListenable: localeController,
+        builder: (c, v, _) {
+          return MaterialApp(
+              theme: ThemeData(fontFamily: 'Noto Sans JP'),
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: v,
+              home: RiskView(
+                risk: risk,
+              ));
+        });
   }
 }
