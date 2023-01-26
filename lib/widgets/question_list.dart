a85b4aab347c7dc02e4ddd8cb9e0aca217eec354
Kazuhiro Miyahara <kmiyahara@studiomexx.org>
2023-01-26 23:58:30 +0900

    add list skelton

---
+++ lib/widgets/question_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class QeustionList extends StatelessWidget {
+  const QeustionList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleList = _buildTitleList(context);
+    final widgetList = _buildWidgetList(context);
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  Widget _buildWidgetList(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+  Widget _buildTitleList(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
