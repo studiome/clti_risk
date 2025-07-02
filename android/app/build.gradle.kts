eae781ca97511feccf7bb31101be298da23aefa6
kmiyahara <kmiyahara@studiomexx.org>
2025-07-02 22:52:19 +0900

    upgrade android api level

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.studiomexx.clti_risk"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 35
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
