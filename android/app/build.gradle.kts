aa8bfe3de58fdd5c607e69648d47f3f3dadb6d04
kmiyahara <kmiyahara@studiomexx.org>
2026-02-17 11:54:12 +0900

    android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 35
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
